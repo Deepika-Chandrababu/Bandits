309eb4c43c2d8fd417758f99fdc8e5ce997716af
Deepika-Chandrababu <50479867+Deepika-Chandrababu@users.noreply.github.com>
2023-08-24 17:16:19 +0200

    Update m5atlucb.sh

--- m5atlucb.sh
+++ m5atlucb.sh
@@ -2,7 +2,7 @@
 
 dir="C:\Users\Public\ouput\m=5"
 
-for r in {13..100}
+for r in {1..100}
 do
    python run_atlucb.py -s $r -t 250 -m 5 -e "agri{ }" > $dir/atlucb.$r.csv
    python postprocess.py -m 5 -e "agri{ }" -c $dir/atlucb.$r.csv -s prop_of_success > "$dir/atlucb-$r.prop_of_success"
